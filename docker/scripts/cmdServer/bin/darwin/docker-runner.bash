01e4857730932c4d8bc3dc55efe373cd73b23313
pavlokulakovskiy <pkulakovsky@gmail.com>
2019-05-10 17:31:09 +0300

    docker-runner.bash updated

--- docker/scripts/cmdServer/bin/darwin/docker-runner.bash
+++ docker/scripts/cmdServer/bin/darwin/docker-runner.bash
@@ -8,14 +8,15 @@ do
 
     if [[ -z "$isGenerate" ]]
      then
-        echo "Nothing to do"
-     else
          #git_url=`echo "$line" | sed -e 's/.*git_url\":\"\(.*\)\"/\1/p'`
          GIT_URL=`echo "$line" | jq '.repository.git_url'`
          GIT_EMAIL=`echo "$line" | jq '.head_commit.committer.email'`
          GIT_NAME=`echo "$line" | jq '.head_commit.committer.name'`
 
         docker run -it --rm -v ${HOME}/.ssh:/root/.ssh -e GIT_URL=${GIT_URL} -e GIT_EMAIL=${GIT_EMAIL} -e GIT_NAME=${GIT_NAME} kupolua/site-builder
+
+     else
+        echo "Nothing to do"
     fi
 
 done < "${1:-/dev/stdin}"
